396b19aa2ed775b791c647f535d0908c1c22ef76
ovolkov <o.volkov@globallogic.com>
2016-04-28 13:06:00 +0300

    ip removed from listen

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ http.createServer(function (req, res) {
     content.push(`path ${req.url}`);
     res.end( content.join('\n') );
     // res.end('Hello from app1!\n' + util.inspect(req));
-}).listen(port, "127.0.0.1");
+}).listen(port);
 console.log('Server running at http://127.0.0.1:' + port);
